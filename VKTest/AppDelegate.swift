317fa7c09edff4c3554c3ff1300c02df683357c8
belannik <belannik@aol.com>
2020-08-10 02:47:38 +0300

    AppDelegate

--- VKTest/AppDelegate.swift
+++ VKTest/AppDelegate.swift
@@ -7,18 +7,61 @@
 //
 
 import UIKit
+import VKSdkFramework
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate
 {
+    // MARK: - Property list
+
+    private let authService = AuthService()
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-
+        authService.delegate = self
         window = UIWindow(frame: UIScreen.main.bounds)
+        let authVC = AuthVC(authService: authService)
+        window?.rootViewController = authVC
         window?.makeKeyAndVisible()
 
         return true
     }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        VKSdk.processOpen(url, fromApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue)
+        return true
+    }
 }
 
+// MARK: - AuthServiceDelegate
+
+extension AppDelegate: AuthServiceDelegate
+{
+    func authServiceShouldShow(_ viewController: UIViewController) {
+        let rootVC = window?.rootViewController
+        rootVC?.present(viewController, animated: true)
+    }
+    
+    func authServiceSignIn() {
+        guard let token = authService.token else { return }
+        let networkService = FeedsNetworkService(token: token)
+        let feedVC = FeedVC(networkService: networkService)
+        let navigationController = LightStatusBarNavigationController(rootViewController: feedVC)
+        navigationController.modalPresentationStyle = .fullScreen
+		navigationController.modalPresentationCapturesStatusBarAppearance = true
+		window?.rootViewController = navigationController
+    }
+    
+    func authServiceDidSignInFail() {
+		window?.rootViewController?.showAlert(title: #function)
+    }
+
+	func vkSdkUserAuthorizationFailed() {
+		window?.rootViewController?.showAlert(title: #function)
+	}
+	
+	func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+		window?.rootViewController?.showAlert(title: #function, message: captchaError.description)
+	}
+	
+}
