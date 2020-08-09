08efb41ef92a9e4290d95dddc067f590c05d58af
belannik <belannik@aol.com>
2020-08-10 02:45:49 +0300

    NavigationController
    will changes in status bar

---
+++ VKTest/Controllers/LightStatusBarNavigationController.swift
@@ -0,0 +1,14 @@
+//
+//  LightNavigationController.swift
+//  VKTest
+//
+//  Created by Anton on 09.08.2020.
+//  Copyright © 2020 Anton. All rights reserved.
+//
+
+import UIKit
+
+final class LightStatusBarNavigationController: UINavigationController
+{
+	override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
+}
