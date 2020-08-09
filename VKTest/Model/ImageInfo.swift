4a13b9ba96d6773c0480df1866aabb6fa03958b4
belannik <belannik@aol.com>
2020-08-10 02:47:11 +0300

    Auxiliary models

---
+++ VKTest/Model/ImageInfo.swift
@@ -0,0 +1,16 @@
+//
+//  ImageInfo.swift
+//  VKTest
+//
+//  Created by Anton on 04.08.2020.
+//  Copyright © 2020 Anton. All rights reserved.
+//
+
+import CoreGraphics
+
+struct ImageInfo
+{
+    let size: CGSize
+    let type: ImageType
+    let path: String
+}
---
+++ VKTest/Model/ImageType.swift
@@ -0,0 +1,15 @@
+//
+//  ImageViewType.swift
+//  VKTest
+//
+//  Created by Anton on 04.08.2020.
+//  Copyright © 2020 Anton. All rights reserved.
+//
+
+import Foundation
+
+enum ImageType: String
+{
+    case photo
+    case video
+}
